e0712a72b36cda3ca285a4b6b2cea0259e1046d6
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-11-27 22:09:02 +0100

    new file:   data/jobs.js 	new file:   data/jobs_test.js 	modified:   index.html 	modified:   script.js

---
+++ data/jobs_test.js
@@ -0,0 +1,7 @@
+const jobs = {
+    job1: { location: "NY, US", title: "java dev" },
+    job2: { location: "Genoa, IT", title: "web dev" },
+    job3: { location: "US", title: "dev" }
+  };
+
+export default jobs;
