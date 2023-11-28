811359fb57a270c8ece91c17d340a491a158226f
CodeKaito <jerome.decinco@maxwell.mi.it>
2023-11-28 16:32:52 +0100

    ✅ Task completed successfully

--- data/jobs_test.js
+++ data/jobs_test.js
@@ -1,7 +1,10 @@
-const jobs = {
-    job1: { location: "NY, US", title: "java dev" },
-    job2: { location: "Genoa, IT", title: "web dev" },
-    job3: { location: "US", title: "dev" }
-  };
+const jobs = [
+    // job 1
+    { location: "NY, US", title: "java dev" },
+    // job 2
+    { location: "Genoa, IT", title: "web dev" },
+    // job 3
+    { location: "US", title: "dev" }
+  ];
 
 export default jobs;
--- script.js
+++ script.js
@@ -1,5 +1,160 @@
+// Import dei dati da db fittizio
 import data from './data/jobs.js';
 import data_test from './data/jobs_test.js';
 
-console.log(data[0]);
-console.log(data_test.job1);
+// Il risultato della funzione searchAlgorithm
+let globalResultObject = null;
+// La tabella del risultato della funzione searchAlgorithm
+let existingTable = null;
+// Dichiarazione della arrow function searchAlgorithm che prende in input sia location che title
+let searchAlgorithm = (location, titleJob) => {
+    // Inizializzo un array d'appoggio per inserire i valori trovati
+    let result = [];
+    // Itero con un for in l'array di oggetti data_test
+    for (const key in data_test) {
+        // Inizializzo una variabile che prenda solo le locations dei singoli oggetti, aggiungo il punto 3 dell'esercizio 1 per il case insensitive
+        const locations = data_test[key].location.toLowerCase();
+
+        // Inizializzo una variabile che prenda solo i titoli delle posizioni lavorative dei singoli oggetti, aggiungo il punto 3 dell'esercizio 1 per il case insensitive
+        const titleJobs = data_test[key].title.toLowerCase();
+
+        // Effettuo un confronto con l'input dell'utente e ció che ho a db per trovare i lavori che soddisfano i requisiti
+        if (locations.includes(location) && titleJobs.includes(titleJob)) {
+
+            // Inserisco all'interno dell'array di appoggio con un push, gli elementi che soddisfano i requisiti
+            result.push(data_test[key]);
+        }
+    }
+
+    // Creo l'oggetto risultato richiesto con result: gli elementi che soddisfano i requisiti e il count con il numero degli elementi
+    const resultObject = {
+        // gli elementi che soddisfano i requisiti
+        result: result,
+        // il numero gli elementi che soddisfano i requisiti
+        count: result.length,
+    };
+    // Salvo il risultato resultObject nella variabile esterna 
+    globalResultObject = resultObject;
+    // Guardo a video se il risultato finale soddisfa i requisiti richiesti
+    console.log(globalResultObject);
+    // Ritorno il risultato in formato array di oggetti alla funzione searchAlgorithm iniziale
+    return resultObject;
+}
+
+// Definizione della funzione per creare il titolo "Result"
+let createTheResultTitle = (num) => {
+    // Creo l'elemento h1 per il titolo "Result"
+    const titleResult = document.createElement("h1");
+    // Aggiungo il valore "Result: " + il count dei risultati
+    titleResult.textContent = "Result: " + num;
+    // Aggiungo la classe result per dargli uno styling
+    titleResult.classList.add("result");
+    // Aggiungo l'elemento in fondo al body del mio foglio di lavoro
+    document.body.appendChild(titleResult);
+}
+
+// TODO: create the result table
+let createTheResultTable = (resultArray) => {
+    // Creazione di una tabella
+    const table = document.createElement("table");
+
+    // Aggiunta di una classe alla tabella per applicare stili CSS
+    table.classList.add("result-table");
+
+    // Creazione di righe e inserimento di testo
+    let headerRow = document.createElement("tr");
+
+    // Creazione di celle e inserimento di testo
+    let headerCell1 = document.createElement("td");
+    headerCell1.textContent = "#";
+    let headerCell2 = document.createElement("td");
+    headerCell2.textContent = "Location";
+    let headerCell3 = document.createElement("td");
+    headerCell3.textContent = "Job Title";
+
+    // Aggiunta delle celle di intestazione alla riga di intestazione
+    headerRow.appendChild(headerCell1);
+    headerRow.appendChild(headerCell2);
+    headerRow.appendChild(headerCell3);
+
+    // Aggiunta della riga di intestazione alla tabella
+    table.appendChild(headerRow);
+
+    // Creo righe in base al numero di oggetti nell'array resultArray
+    for (let i = 0; i < resultArray.length; i++) {
+        // Creazione di una nuova riga per ciascun risultato
+        let row = document.createElement("tr");
+
+        // Creazione di celle e inserimento di testo
+        let cell1 = document.createElement("td");
+        cell1.textContent = (i + 1).toString(); // Numero di riga
+        let cell2 = document.createElement("td");
+        cell2.textContent = resultArray[i].location;
+        let cell3 = document.createElement("td");
+        cell3.textContent = resultArray[i].title;
+
+        // Aggiunta delle celle alla riga
+        row.appendChild(cell1);
+        row.appendChild(cell2);
+        row.appendChild(cell3);
+
+        // Aggiunta della riga alla tabella
+        table.appendChild(row);
+    }
+
+    // Aggiunta della tabella al corpo del documento
+    document.body.appendChild(table);
+
+    // Aggiorno la variabile existingTable con la nuova tabella
+    existingTable = table;
+}
+
+const resultArray = [
+    { location: 'NY, US', title: 'java dev' },
+    { location: 'US', title: 'dev' }
+];
+
+const resultObject = {
+    result: resultArray,
+    count: resultArray.length,
+};
+
+
+// Inserisco in una variabile il pulsante button
+let submit = document.getElementById('cerca');
+// Richiamo l'addEventListener per richiamare la funzione
+submit.addEventListener('click', async() => {
+    // Identifico i valori dell'input della location
+    let location = document.getElementById('location').value;
+
+    // Identifico i valori dell'input del job title
+    let jobTitle = document.getElementById('jobTitle').value;
+
+    // Verifico se gli input sono vuoti, se lo sono, voglio che la function si fermi e che la pagina mi dia un'errore
+    if (location === '' || jobTitle === '') {
+        alert("Error! Insert both values!");
+        // Interrompe l'esecuzione dell'intera funzione
+        return;
+    }
+    // Chiamo la searchAlgorithm e awaito che sia completata
+    const resultObject = await searchAlgorithm(location, jobTitle);
+    
+    // Verifico se il titolo é giá presente sul foglio di lavoro
+    if (!document.querySelector('.result')) {
+        // Stampo sul foglio di lavoro il count dei risultati trovati
+        createTheResultTitle(resultObject.count);
+    };
+    
+    // Ottieni la tabella esistente
+    const existingTable = document.querySelector('table');
+
+    // Verifica se la tabella esiste già e se è diversa dalla precedente (outerHTML)
+    if (existingTable && existingTable.outerHTML === document.getElementsByTagName('table')[0].outerHTML) {
+        // Se è uguale, rimuovi la tabella
+        existingTable.remove();
+    }
+
+    // Crea la tabella con i risultati
+    createTheResultTable(resultObject.result);
+});
+
